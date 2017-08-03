4c55787bffb0e6329540494542b08b0eec7a4b06
Dmitriy Beglov <ditransler@gmail.com>
2017-08-03 22:45:45 +0400

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "plugins": ["html"],
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-console": "warn",
+        "no-unused-vars": 1
+    }
+};
---
+++ Readme.md
@@ -0,0 +1,7 @@
+![](https://javascript30.com/images/JS3-social-share.png)
+
+# Edu project for "JavaScript30" course by Wes Bos
+
+Here I'll try out everything Wes Bos teaches in the course [https://JavaScript30.com](https://JavaScript30.com)
+
+The source repo with examples for the course - [link](https://github.com/wesbos/JavaScript30)
